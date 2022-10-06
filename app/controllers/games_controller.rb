1d9e6f509f8854b97b53f4d176668f941183af95
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-06 18:37:13 -0500

    add helper methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,25 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { (('a'..'z').to_a).sample }
   end
 
   def score
-    # raise
+    @word = params[:word]
+    @letters = params[:letters].split(' ')
+    @valid = valid_word?(@word, @letters)
+    @english = english_word?(@word)
+  end
+
+  private
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    JSON.parse(URI.open(url).read)['found']
+  end
+
+  def valid_word?(word, grid)
+    word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
   end
 end
