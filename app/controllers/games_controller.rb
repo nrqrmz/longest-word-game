ebcd8ffa56753842feb04d45f5958eb4f143725d
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-06 17:35:58 -0500

    add and style array of random letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   def new
+    @letters = Array.new(10) { (('a'..'z').to_a).sample }
   end
 
   def score
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'games/new'
+  get 'new', to: 'games#new'
   get 'games/score'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
